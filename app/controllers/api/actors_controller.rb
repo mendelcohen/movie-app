aa8ab79c3467873d88aff1e5f47a2ee4382bcc21
Mendel Cohen <mmhakohen@gmail.com>
2020-09-14 01:13:43 -0400

    adds query, url and body param paths

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,6 +1,16 @@
 class Api::ActorsController < ApplicationController
-  def single_actor
+  def actor
     @actor = Actor.find_by(id: 6)
     render "single.json.jb"
   end
+
+  def actor
+    if params["id"].to_i < 6
+      @actor = "choose an id higher than 5"
+    else
+      @actor = Actor.find_by(id: params["id"])
+    end
+    render "single.json.jb"
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/actors_path" => "actors#single_actor"
+    get "/actors" => "actors#actor"
     get "/all_movies_path" => "movies#all_movies"
     get "/first_movie_path" => "movies#first_movie"
     get "/take_movie_path" =>"movies#take_movie"
+    get "/query_param" => "actors#actor"
+    get "/url_segment/:id" => "actors#actor"
+    post "/actors" => "actors#actor"
   end
 end
