6bd0b5058484c5f5ada08686c2d0b4173ecfe365
Mendel Cohen <mmhakohen@gmail.com>
2020-09-26 23:50:06 -0400

    adds genres and MovieGenre table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,10 @@ class Movie < ApplicationRecord
   validates :director, length: { minimum: 5 }, presence: true
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  def genre_names
+    genres.map {|genre| genre[:name]}
+  end
 end
